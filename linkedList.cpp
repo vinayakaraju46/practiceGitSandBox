d447e093aef89e1a3d3f1015e2ee5f77190a3468
vinayakaraju46 <vinayakaraju46@gmail.com>
2022-04-05 13:03:47 +0530

    First commit of LinkedList

---
+++ linkedList.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(const int& val) {
+            data = val;
+            next = NULL;
+        }
+};
+
+
+int main() {
+
+}
